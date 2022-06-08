526cfa8c85c28fcc9f59472363503d51507ac55c
github yuya-lock <yuyahappy1009@gmail.com>
2022-06-08 20:40:23 +0900

    課題４

---
+++ simurate4.m
@@ -0,0 +1,29 @@
+O = 7;
+N = 2^O-1;
+pattern1 = prbs(O,N);
+
+ts = 25;
+data_per_ts = 32;
+data_count = N * data_per_ts;
+
+for i = 1:N-1
+    for j = 1:data_per_ts
+        if pattern1(i+1) == pattern1(i)
+             pattern2(i*data_per_ts+j) = pattern1(i);
+        else
+            if pattern1(i) == 1
+                pattern2(i*data_per_ts+j) = cos((pi/2)*(j/data_per_ts));
+            else
+                pattern2(i*data_per_ts+j) = cos(-((pi/2)*(1-j/data_per_ts)));
+            end
+        end
+    end
+end
+
+y = fft(pattern2);
+power = fftshift(10 * log10(abs(y) / data_count));
+
+center_freq = data_count / 2 + 1;
+freq_diff = (0:data_count-1) ./ data_per_ts - (N/2);
+
+plot(freq_diff, power)
